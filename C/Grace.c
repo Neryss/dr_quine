bdc671de75eb127f1377d2a51d8e76aae9fbd5c4
neryss <ckurt@neryss.pw>
2022-10-24 23:46:18 +0200

    grace done C

--- C/Grace.c
+++ C/Grace.c
@@ -1,3 +1,3 @@
 #include <stdio.h>
-#define _main() int	main(){FILE	*fptr;fptr = fopen("Grace_kid.c", "w");char	*f = "#include <stdio.h>%c#define _main() int	main(){FILE	*fptr;fptr = fopen(%cGrace_kid.c%c, %cw%c);char	*f = %c%s%c;fprintf(fptr, f, 34, f, 34);fclose(fptr);return(0);}";fprintf(fptr, f, 10, 34, 34, 34, 34, 34, f, 34);fclose(fptr);return(0);}
+#define _main() int	main(){FILE	*fptr;fptr = fopen("Grace_kid.c", "w");char	*f = "#include <stdio.h>%c#define _main() int	main(){FILE	*fptr;fptr = fopen(%cGrace_kid.c%c, %cw%c);char	*f = %c%s%c;fprintf(fptr, f, 10, 34, 34, 34, 34, 34, f, 34, 10);fclose(fptr);return(0);}%c_main();";fprintf(fptr, f, 10, 34, 34, 34, 34, 34, f, 34, 10);fclose(fptr);return(0);}
 _main();
