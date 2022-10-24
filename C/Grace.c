1f792e87d2e220f3948678ae339125f038474408
neryss <neryssistos@gmail.com>
2022-10-24 19:35:26 +0200

    grace need to add inline macro

--- C/Grace.c
+++ C/Grace.c
@@ -9,8 +9,8 @@ int	main()
 
 	FILE	*fptr;
 	fptr = fopen("Grace_kid.c", "w");
-	char	*f = "#include <stdio.h>\n\nint	main()\n{\n	/*\n		comment\n	*/\n\n	FILE	*fptr;\n	fptr = fopen(\"Grace_kid.c\", \"w\");\n	return(0);\n}";
-	fprintf(fptr, "%s", f);
+	char	*f = "#include <stdio.h>%c%cint	main()%c{%c	/*%c		comment%c	*/%c%c	FILE	*fptr;%c	fptr = fopen(%cGrace_kid.c%c, %cw%c);%c	char	*f = %c%s%c;%c	fprintf(fptr, f, 34, f, 34);%c	fclose(fptr);%c	return(0);%c}";
+	fprintf(fptr, f, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, 34, 34, 34, 10, 34, f, 34, 10, 10, 10, 10);
 	fclose(fptr);
 	return(0);
 }
--- C/Grace_kid.c
+++ C/Grace_kid.c
@@ -8,5 +8,8 @@ int	main()
 
 	FILE	*fptr;
 	fptr = fopen("Grace_kid.c", "w");
+	char	*f = "#include <stdio.h>%c%cint	main()%c{%c	/*%c		comment%c	*/%c%c	FILE	*fptr;%c	fptr = fopen(%cGrace_kid.c%c, %cw%c);%c	char	*f = %c%s%c;%c	fprintf(fptr, f, 34, f, 34);%c	fclose(fptr);%c	return(0);%c}";
+	fprintf(fptr, f, 34, f, 34);
+	fclose(fptr);
 	return(0);
 }
