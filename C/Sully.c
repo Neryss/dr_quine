c2a30a19838613fed694422adc654fe60173702f
neryss <neryssistos@gmail.com>
2022-10-31 00:30:44 +0100

    segfault because of buff size?

--- C/Sully.c
+++ C/Sully.c
@@ -6,13 +6,13 @@ int	main()
 	if (i >= 0)
 	{
 		--i;
-		char	buff[64];
+		char	buff[1024];
 		snprintf(buff, sizeof(buff), "Sully_%d.c", i);
 		FILE	*fptr = fopen(buff, "w");
-		(void)fptr;
-		char *f = "#include <stdio.h>%c#include <stdlib.h>%cint i = %d%cint main()%c{%c	if (i >= 0)%c	{%c		i--;%c		char	buff[64];%c		snprintf(buff, sizeof(buff), %cSully_%d.c%c, i);%c		FILE	*fptr = fopen(buff, %cw%c);%c		char *f = %c%s%c;%cfprintf(fptr, f, 10, 10, 10, i, 10, 10, 10, 10, 10, 10, 10, 34, i, 34, 10, 34, 34, 10, 34, f, 34, 10, 10, 10);%c	}%c	return(0);}";
-		(void)f;
-		fprintf(fptr, "%s", f);
+		// (void)fptr;
+		char *f = "#include <stdio.h>%c#include <stdlib.h>%cint i = %d;%cint main()%c{%c	if (i >= 0)%c	{%c		i--;%c		char	buff[1024];%c		snprintf(buff, sizeof(buff), %cSully_%d.c%c, i);%c		FILE	*fptr = fopen(buff, %cw%c);%c		char *f = %c%s%c;%c		fprintf(fptr, f, 10, 10, 10, i, 10, 10, 10, 10, 10, 10, 10, 34, i, 34, 10, 34, 34, 10, 34, f, 34, 10, 10, 10);%c	}%c	return(0);%c}";
+		// (void)f;
+		fprintf(fptr, f, 10, 10, i, 10, 10, 10, 10, 10, 10, 10, 34, i, 34, 10, 34, 34, 10, 34, f, 34, 10, 10, 10, 10);
 		fclose(fptr);
 		// system("clang -Wall -Wextra -Werror -o Sully_kid.c Sully.c");
 	}
--- C/Sully_4.c
+++ C/Sully_4.c
@@ -1,1 +1,16 @@
-#include <stdio.h>%c#include <stdlib.h>%cint i = %d%cint main()%c{%c	if (i >= 0)%c	{%c		i--;%c		char	buff[64];%c		snprintf(buff, sizeof(buff), %cSully_%d.c%c, i);%c		FILE	*fptr = fopen(buff, %cw%c);%c		char *f = %c%s%c;%cfprintf(fptr, f, 10, 10, 10, i, 10, 10, 10, 10, 10, 10, 10, 34, i, 34, 10, 34, 34, 10, 34, f, 34, 10, 10, 10);%c	}%c	return(0);}
+#include <stdio.h>
+#include <stdlib.h>
+int i = 4;
+int main()
+{
+	if (i >= 0)
+	{
+		i--;
+		char	buff[1024];
+		snprintf(buff, sizeof(buff), "Sully_4.c", i);
+		FILE	*fptr = fopen(buff, "w");
+		char *f = "#include <stdio.h>%c#include <stdlib.h>%cint i = %d;%cint main()%c{%c	if (i >= 0)%c	{%c		i--;%c		char	buff[1024];%c		snprintf(buff, sizeof(buff), %cSully_%d.c%c, i);%c		FILE	*fptr = fopen(buff, %cw%c);%c		char *f = %c%s%c;%c		fprintf(fptr, f, 10, 10, 10, i, 10, 10, 10, 10, 10, 10, 10, 34, i, 34, 10, 34, 34, 10, 34, f, 34, 10, 10, 10);%c	}%c	return(0);%c}";
+		fprintf(fptr, f, 10, 10, 10, i, 10, 10, 10, 10, 10, 10, 10, 34, i, 34, 10, 34, 34, 10, 34, f, 34, 10, 10, 10);
+	}
+	return(0);
+}
