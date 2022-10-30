9670b413c226e6b51b2ea1df0d8b72a63504fa10
neryss <neryssistos@gmail.com>
2022-10-31 00:04:05 +0100

    seg fault

--- C/Grace.c
+++ C/Grace.c
@@ -1,3 +1,12 @@
 #include <stdio.h>
-#define _main() int	main(){FILE	*fptr;fptr = fopen("Grace_kid.c", "w");char	*f = "#include <stdio.h>%c#define _main() int	main(){FILE	*fptr;fptr = fopen(%cGrace_kid.c%c, %cw%c);char	*f = %c%s%c;fprintf(fptr, f, 10, 34, 34, 34, 34, 34, f, 34, 10);fclose(fptr);return(0);}%c_main();";fprintf(fptr, f, 10, 34, 34, 34, 34, 34, f, 34, 10);fclose(fptr);return(0);}
+#define _main()                                                                                                                                                                                                              \
+	int main()                                                                                                                                                                                                               \
+	{                                                                                                                                                                                                                        \
+		FILE *fptr;                                                                                                                                                                                                          \
+		fptr = fopen("Grace_kid.c", "w");                                                                                                                                                                                    \
+		char *f = "#include <stdio.h>%c#define _main() int	main(){FILE	*fptr;fptr = fopen(%cGrace_kid.c%c, %cw%c);char	*f = %c%s%c;fprintf(fptr, f, 10, 34, 34, 34, 34, 34, f, 34, 10);fclose(fptr);return(0);}%c_main();"; \
+		fprintf(fptr, f, 10, 34, 34, 34, 34, 34, f, 34, 10);                                                                                                                                                                 \
+		fclose(fptr);                                                                                                                                                                                                        \
+		return (0);                                                                                                                                                                                                          \
+	}
 _main();
--- C/Sully.c
+++ C/Sully.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 int	i = 5;
-
 int	main()
 {
 	if (i >= 0)
 	{
-		system("clang -Wall -Wextra -Werror -o Sully_kid.c Sully.c");
+		--i;
+		char	buff[64];
+		snprintf(buff, sizeof(buff), "Sully_%d.c", i);
+		FILE	*fptr = fopen(buff, "w");
+		char *f = "#include <stdio.h>%c#include <stdlib.h>%cint i = %d%cint main()%c{%c	if (i >= 0)%c	{%c		i--;%c		char	buff[64];%c		snprintf(buff, sizeof(buff), %cSully_%d.c%c, i);%c		FILE	*fptr = fopen(buff, %cw%c);%c		char *f = %c%s%c;%cfprintf(fptr, f, 10, 10, 10, i, 10, 10, 10, 10, 10, 10, 10, 34, i, 34, 10, 34, 34, 10, 34, f, 34, 10, 10, 10);%c	}%c	return(0);}";
+		fprintf(fptr, f, 10, 10, 10, i, 10, 10, 10, 10, 10, 10, 10, 34, i, 34, 10, 34, 34, 10, 34, f, 34, 10, 10, 10);
+		// system("clang -Wall -Wextra -Werror -o Sully_kid.c Sully.c");
 	}
 	return(0);
 }
