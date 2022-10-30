5f9818de57c0723a6ce87ef7c5300ddb41f1c0cd
neryss <neryssistos@gmail.com>
2022-10-31 00:22:34 +0100

    duck seg

---
+++ C/Sully_4.c
@@ -0,0 +1,1 @@
+#include <stdio.h>%c#include <stdlib.h>%cint i = %d%cint main()%c{%c	if (i >= 0)%c	{%c		i--;%c		char	buff[64];%c		snprintf(buff, sizeof(buff), %cSully_%d.c%c, i);%c		FILE	*fptr = fopen(buff, %cw%c);%c		char *f = %c%s%c;%cfprintf(fptr, f, 10, 10, 10, i, 10, 10, 10, 10, 10, 10, 10, 34, i, 34, 10, 34, 34, 10, 34, f, 34, 10, 10, 10);%c	}%c	return(0);}
