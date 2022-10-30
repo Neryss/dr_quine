dfc809ccf5cf3c1e628c78986435a06e4d4f10a1
neryss <neryssistos@gmail.com>
2022-10-31 00:35:51 +0100

    need to find a dodge for i

--- C/Sully.c
+++ C/Sully.c
@@ -10,9 +10,9 @@ int	main()
 		snprintf(buff, sizeof(buff), "Sully_%d.c", i);
 		FILE	*fptr = fopen(buff, "w");
 		// (void)fptr;
-		char *f = "#include <stdio.h>%c#include <stdlib.h>%cint i = %d;%cint main()%c{%c	if (i >= 0)%c	{%c		i--;%c		char	buff[1024];%c		snprintf(buff, sizeof(buff), %cSully_%d.c%c, i);%c		FILE	*fptr = fopen(buff, %cw%c);%c		char *f = %c%s%c;%c		fprintf(fptr, f, 10, 10, 10, i, 10, 10, 10, 10, 10, 10, 10, 34, i, 34, 10, 34, 34, 10, 34, f, 34, 10, 10, 10);%c	}%c	return(0);%c}";
+		char *f = "#include <stdio.h>%c#include <stdlib.h>%cint i = %d;%cint main()%c{%c	if (i >= 0)%c	{%c		i--;%c		char	buff[1024];%c		snprintf(buff, sizeof(buff), %cSully_%s.c%c, i);%c		FILE	*fptr = fopen(buff, %cw%c);%c		char *f = %c%s%c;%c		fprintf(fptr, f, 10, 10, 10, i, 10, 10, 10, 10, 10, 10, 10, 34, i, 34, 10, 34, 34, 10, 34, f, 34, 10, 10, 10, 10);%c		fclose(fptr);%c	}%c	return(0);%c}";
 		// (void)f;
-		fprintf(fptr, f, 10, 10, i, 10, 10, 10, 10, 10, 10, 10, 34, i, 34, 10, 34, 34, 10, 34, f, 34, 10, 10, 10, 10);
+		fprintf(fptr, f, 10, 10, i, 10, 10, 10, 10, 10, 10, 10, 34, "%d", 34, 10, 34, 34, 10, 34, f, 34, 10, 10, 10, 10, 10);
 		fclose(fptr);
 		// system("clang -Wall -Wextra -Werror -o Sully_kid.c Sully.c");
 	}
--- C/Sully_4.c
+++ C/Sully_4.c
@@ -7,10 +7,11 @@ int main()
 	{
 		i--;
 		char	buff[1024];
-		snprintf(buff, sizeof(buff), "Sully_4.c", i);
+		snprintf(buff, sizeof(buff), "Sully_%d.c", i);
 		FILE	*fptr = fopen(buff, "w");
-		char *f = "#include <stdio.h>%c#include <stdlib.h>%cint i = %d;%cint main()%c{%c	if (i >= 0)%c	{%c		i--;%c		char	buff[1024];%c		snprintf(buff, sizeof(buff), %cSully_%d.c%c, i);%c		FILE	*fptr = fopen(buff, %cw%c);%c		char *f = %c%s%c;%c		fprintf(fptr, f, 10, 10, 10, i, 10, 10, 10, 10, 10, 10, 10, 34, i, 34, 10, 34, 34, 10, 34, f, 34, 10, 10, 10);%c	}%c	return(0);%c}";
-		fprintf(fptr, f, 10, 10, 10, i, 10, 10, 10, 10, 10, 10, 10, 34, i, 34, 10, 34, 34, 10, 34, f, 34, 10, 10, 10);
+		char *f = "#include <stdio.h>%c#include <stdlib.h>%cint i = %d;%cint main()%c{%c	if (i >= 0)%c	{%c		i--;%c		char	buff[1024];%c		snprintf(buff, sizeof(buff), %cSully_%s.c%c, i);%c		FILE	*fptr = fopen(buff, %cw%c);%c		char *f = %c%s%c;%c		fprintf(fptr, f, 10, 10, 10, i, 10, 10, 10, 10, 10, 10, 10, 34, i, 34, 10, 34, 34, 10, 34, f, 34, 10, 10, 10, 10);%c		fclose(fptr);%c	}%c	return(0);%c}";
+		fprintf(fptr, f, 10, 10, 10, i, 10, 10, 10, 10, 10, 10, 10, 34, i, 34, 10, 34, 34, 10, 34, f, 34, 10, 10, 10, 10);
+		fclose(fptr);
 	}
 	return(0);
 }
