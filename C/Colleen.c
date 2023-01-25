633d69261247e1a18a0eeca97e6e6e1b29199d2f
neryss <neryssistos@gmail.com>
2023-01-25 23:28:23 +0100

    patch function call in colleen

--- C/Colleen.c
+++ C/Colleen.c
@@ -11,7 +11,8 @@ int	main(void)
 	/*
 		inside
 	*/
-	char	*f = "#include <stdio.h>%c%c/*%c	I'm outside%c*/%c%cvoid	dum(void){return;}%c%cint	main(void)%c{%c	/*%c		inside%c	*/%c	char	*f = %c%s%c;%c	printf(f, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, f, 34, 10, 10, 10);%c	return (0);%c}";
-	printf(f, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, f, 34, 10, 10, 10);
+	dum();
+	char	*f = "#include <stdio.h>%c%c/*%c	I'm outside%c*/%c%cvoid	dum(void){return;}%c%cint	main(void)%c{%c	/*%c		inside%c	*/%c	dum();%c	char	*f = %c%s%c;%c	printf(f, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, f, 34, 10, 10, 10);%c	return (0);%c}";
+	printf(f, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, f, 34, 10, 10, 10);
 	return (0);
 }
