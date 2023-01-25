0dd71fc6d28cac6632c46edf7fe964fba5b82dc8
neryss <neryssistos@gmail.com>
2023-01-26 00:06:21 +0100

    clearer macro

--- C/Grace.c
+++ C/Grace.c
@@ -1,3 +1,3 @@
 #include <stdio.h>
-#define _main() int	main(){FILE	*fptr;fptr = fopen("Grace_kid.c", "w");char	*f = "#include <stdio.h>%c#define _main() int	main(){FILE	*fptr;fptr = fopen(%cGrace_kid.c%c, %cw%c);char	*f = %c%s%c;fprintf(fptr, f, 10, 34, 34, 34, 34, 34, f, 34, 10);fclose(fptr);return(0);}%c_main();";fprintf(fptr, f, 10, 34, 34, 34, 34, 34, f, 34, 10);fclose(fptr);return(0);}
-_main();
+#define _MAIN() int	main(){FILE	*fptr;/*hey*/fptr = fopen("Grace_kid.c", "w");char	*f = "#include <stdio.h>%c#define _MAIN() int	main(){FILE	*fptr;/*hey*/fptr = fopen(%cGrace_kid.c%c, %cw%c);char	*f = %c%s%c;fprintf(fptr, f, 10, 34, 34, 34, 34, 34, f, 34, 10);fclose(fptr);return(0);}%c_MAIN();";fprintf(fptr, f, 10, 34, 34, 34, 34, 34, f, 34, 10);fclose(fptr);return(0);}
+_MAIN();
