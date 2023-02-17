de287a812ada70f6002d6815128980b4758a1eda
neryss <neryssistos@gmail.com>
2023-02-17 16:14:25 +0100

    asm exemple

--- ASM/test.asm
+++ ASM/test.asm
@@ -1,19 +1,16 @@
-section .data
-	hello:     db 'Hello world!',10    ; 'Hello world!' plus a linefeed character
-	helloLen:  equ $-hello             ; Length of the 'Hello world!' string
-	                                   ; (I'll explain soon)
-
 section .text
 	global _start
 
 _start:
-	mov eax,4            ; The system call for write (sys_write)
-	mov ebx,1            ; File descriptor 1 - standard output
-	mov ecx,hello        ; Put the offset of hello in ecx
-	mov edx,helloLen     ; helloLen is a constant, so we don't need to say
-	                     ;  mov edx,[helloLen] to get it's actual value
-	int 80h              ; Call the kernel
+	mov	eax, 4		; system call for write
+	mov	ebx, 1		; file descriptor
+	mov	ecx, msg	; put offset of hello in ecx (arg for write)
+	mov	edx, len	; len as argument
+	int	80h			; Call the kernel to write
+	mov	eax, 1		; syscall for exit
+	mov	ebx, 0		; return 0
+	int	80h			; call kernel to exit
 
-	mov eax,1            ; The system call for exit (sys_exit)
-	mov ebx,0            ; Exit with return code of 0 (no error)
-	int 80h
+section .data
+msg:	db "Hello world!", 10
+len:	equ $-msg
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Dr_quine
 
 A basic introduction to self replication from 42.
+
+## Resources
+- https://filippo.io/linux-syscall-table/
