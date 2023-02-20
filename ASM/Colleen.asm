1c2dcf4b78eb07296ad7c3fb6ded3132a2c8668a
neryss <neryssistos@gmail.com>
2023-02-20 17:48:09 +0100

    trying out things

--- ASM/Colleen.asm
+++ ASM/Colleen.asm
@@ -2,11 +2,16 @@
 global main
 extern printf
 section .text
-main:
-	push	rbx
-	mov		rdi, test
+
+print:
+	mov	rdi, test
+	mov	rsi, 2
 	call	printf WRT ..plt
 	ret
+main:
+	push	rbx
+	jmp		print
+	ret
 section .data
-test: db "This is a test"
+test: db "This is a test%d"
 
