d5857cb069d11a3a19cf64fe13e7b9d67037abb9
neryss <neryssistos@gmail.com>
2023-02-21 17:26:01 +0100

    wip file open

---
+++ ASM/Grace.s
@@ -0,0 +1,23 @@
+%macro _main 0
+global main
+extern fprintf
+extern open
+extern close
+open_file:
+	push	rbx
+	mov		rax, 2
+	mov		rdi, filename
+	mov		rsi, 2
+	syscall
+	pop		rbx
+	ret
+main:
+	push	rbx
+	pop		rbx
+	jmp		open_file
+	ret
+section .data
+filename: db "Grace_kid.c"
+%endmacro
+
+_main
