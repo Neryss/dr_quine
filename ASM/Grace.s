ade3df57c06cb5ef8a8e764464b0c3063df38d2d
neryss <neryssistos@gmail.com>
2023-02-22 16:14:58 +0100

    ????

--- ASM/Grace.s
+++ ASM/Grace.s
@@ -1,23 +1,23 @@
 global main
-extern fprintf
-extern open
+extern dprintf
 extern close
 %macro _main 0
 main:
 	push	rbx
 	call	open_file
-	push	rax
-	mov		rdi, rax
-	mov		rax, 1
-	lea		rsi, [rel filename]
-	mov		rdx, len
-	syscall
-	pop		rax
+	call	write_to_file
 	call	close_file
 	mov		rax, 0
 	pop		rbx
 	ret
 %endmacro
+write_to_file:
+	push	rbx
+	lea		rsi, [rel code]
+	mov		rdx, 10
+	call	dprintf WRT ..plt
+	pop		rbx
+	ret
 open_file:
 	push	rbx
 	mov		rax, 2
@@ -37,4 +37,5 @@ close_file:
 _main
 section .data
 filename: db "Grace_kid.s"
+code: db "This is a test%1$c"
 len: equ $-filename
