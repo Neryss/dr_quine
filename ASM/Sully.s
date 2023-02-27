f40e1e1acab2a8600b007a818b5b67b46839ff0f
neryss <neryssistos@gmail.com>
2023-02-27 18:02:59 +0100

    rework kinda working

--- ASM/Sully.s
+++ ASM/Sully.s
@@ -41,27 +41,26 @@ open_file:
 	mov		rsi, 64 + 1
 	mov		rdx, 0644o
 	syscall
+	mov		r13, rax
 write_to_file:
 	mov		rdi, r13
-	lea		rsi, [rel code]
+	mov		rsi, [code]
 	mov		rdx, 10
-	;mov		rcx, 34
-	;lea		r8, [rel code]
+	mov		rcx, 34
+	;mov		r8, [code]
 	call	dprintf
-	mov		rax, 60
-	mov		rdi, 0
-	syscall
-make:
-	lea		rdi, [rel exec]
-	call	system
-	ret
+;make:
+;	lea		rdi, [rel exec]
+;	call	system
+;	ret
 close_file:
 	mov		rdi, r13
 	mov		rax, 3
 	syscall
-	ret
+	mov		rax, 60
+	mov		rdi, 0
+	syscall
 section .data
 filename: db "Sully_%d.s", 0
-buffer: db "Sully_x.s", 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 code: db "global main%1$csection .text%1$cmain:%1$c	push	rbx%1$c	pop		rbx%1$c	ret", 0
 exec: db "echo hey", 0
